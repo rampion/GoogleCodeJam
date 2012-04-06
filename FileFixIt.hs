438fa83699bb7e741a735f3265d2ca6d9872baa8
Noah Luck Easterly <noah.easterly@gmail.com>
2012-04-05 23:07:44 -0400

    works

--- FileFixIt.hs
+++ FileFixIt.hs
@@ -6,12 +6,16 @@ import Text.Parsec
 import Text.Parsec.Char
 import Text.Parsec.Combinator
 
-import Control.Applicative hiding (many)
+import Control.Applicative hiding (many, empty)
+import Control.Monad.Writer
+import Data.Map
+import Prelude hiding (lookup)
 
 main :: IO ()
 main = parseAndSolve parseCases $ uncurry fileFixIt
 
-type Path = [String]
+type Dir = String
+type Path = [Dir]
 readPath :: Parsec String u Path
 readPath = char '/' >> many (noneOf "/\n") `sepBy` char '/'
 
@@ -24,7 +28,21 @@ parseCases = do
     existing    <- count numExisting (readPath <* newline)
     desired     <- count numDesired (readPath <* newline)
     return (existing, desired)
-  return []
 
+newtype PathTrie = PathTrie (Map Dir PathTrie)
+
+wrap :: Monad m => Map Dir PathTrie -> m PathTrie
+wrap = return . PathTrie
+
+build :: Path -> PathTrie -> Writer (Sum Integer) PathTrie
+build []     pt           = return pt
+build (d:ds) (PathTrie t) = wrap . (insert d `flip` t) 
+                            =<< build ds 
+                            =<< maybe (tell (Sum 1) >> wrap empty) return (lookup d t)
+
+buildAll :: [Path] -> PathTrie -> Writer (Sum Integer) PathTrie
+buildAll = flip . foldM $ flip build
+  
 fileFixIt :: [Path] -> [Path] -> Integer
-fileFixIt exiting desired = 0
+fileFixIt existing desired = getSum . execWriter $ 
+  buildAll desired =<< const mempty `censor`buildAll existing (PathTrie empty)
